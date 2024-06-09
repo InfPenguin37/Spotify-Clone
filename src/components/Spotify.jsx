e590b9d298ca7fcb5c360cb649236d7a1622ec05
Penguin37 <snhjoy10@gmail.com>
2024-06-09 17:58:38 +0530

    Added basic components with basic styles

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import styled from "styled-components";
+import Sidebar from "./Sidebar";
+import Body from './Body';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 export default function Spotify() {
     return (
-        <div>Spotify</div>
+        <Container>
+            <div className="spotify_body">
+                <Sidebar/>
+                <div className="body">
+                    <Navbar/>
+                    <div className="body_cotents">
+                        <Body/>
+                    </div>
+                </div>
+            </div>
+            <div className="spotify_footer">
+                <Footer/>
+            </div>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    max-width: 100vw;
+    max-height: 100vh;
+    overflow: hidden;
+    display: grid;
+    grid-template-rows: 85vh 15vh;
+    .spotify_body{
+        display: grid;
+        grid-template-columns: 15vw 85vw;
+        height: 100%;
+        width: 100%;
+        background: linear-gradient(transparent, rgba(0, 0, 0, 1));
+        background-color: rgb(32, 87, 100);
+        .body {
+            height: 100%;
+            width: 100%;
+            overflow: auto;
+        }
+    }
+`;
